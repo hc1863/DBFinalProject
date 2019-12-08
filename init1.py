78c6243387f58590e690fec290ebbddd408b4e3e
huddo-c <hc1863@nyu.edu>
2019-12-08 19:23:12 +0800

    dunno

--- init1.py
+++ init1.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 #testchange 1
 
 #Configure MySQL
-conn = pymysql.connect(host='192.168.64.3',
+conn = pymysql.connect(host='192.168.64.2',
                        user='root',
                        password='admin',
                        database='blog')
